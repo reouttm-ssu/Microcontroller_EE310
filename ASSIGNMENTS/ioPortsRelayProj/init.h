094b0f5ec14e26aad72e47953447c61de7ecebb7
reouttm-ssu <reouttm@sonoma.edu>
2026-04-21 13:09:34 -0700

    Update init.h

--- ASSIGNMENTS/ioPortsRelayProj/init.h
+++ ASSIGNMENTS/ioPortsRelayProj/init.h
@@ -21,16 +21,16 @@
 
 // ===================== OUTPUT PINS =====================
 // System LED: turns on when system has power
-#define SYS_LED_LAT   LATDbits.LATD0
+#define SYS_LED_LAT   LATBbits.LATB3
 // Motor output: turns on if code is correct
-#define MOTOR_LAT     LATDbits.LATD1
+#define MOTOR_LAT     LATBbits.LATB4
 // Buzzer output: turns on if code is wrong or emergency happens
-#define BUZZER_LAT    LATDbits.LATD2
+#define BUZZER_LAT    LATBbits.LATB5
 
 // TRIS bits for outputs
-#define SYS_LED_TRIS  TRISDbits.TRISD0
-#define MOTOR_TRIS    TRISDbits.TRISD1
-#define BUZZER_TRIS   TRISDbits.TRISD2
+#define SYS_LED_TRIS  TRISBbits.TRISB3
+#define MOTOR_TRIS    TRISBbits.TRISB4
+#define BUZZER_TRIS   TRISBbits.TRISB5
 
 // ===================== INPUT PINS =====================
 // PR1 and PR2 are the touchless switch inputs
@@ -38,6 +38,7 @@
 #define PR1_PORT      PORTBbits.RB1
 #define PR2_PORT      PORTBbits.RB2
 #define EMG_PORT      PORTBbits.RB0
+#define CHECK_PORT    PORTBbits.RB6
 
 // TRIS bits for input pins
 #define PR1_TRIS      TRISBbits.TRISB1
@@ -45,11 +46,12 @@
 
 // Emergency switch input
 #define EMG_TRIS      TRISBbits.TRISB0
+#define CHECK_TRIS    TRISBbits.TRISB6
 
 // ===================== 7-SEGMENT DISPLAY =====================
 // Entire 7-segment display connected to PORTC
-#define SEG_LAT       LATC
-#define SEG_TRIS      TRISC
+#define SEG_LAT       LATD
+#define SEG_TRIS      TRISD
 
 // Function prototypes
 void setup_pins(void);
